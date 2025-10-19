4143105b46cf1c0ca79ad145017d250e5536426b
ddhi7 <tjsanf211323@gmail.com>
2025-10-19 23:12:47 +0900

    chore : 응답구조 변경

--- src/api/s3Api.ts
+++ src/api/s3Api.ts
@@ -1,4 +1,3 @@
-import axios from "axios";
 import { ApiClient } from './apiClient';
 import { UploadResponse } from '../types/s3';
 import { ApiResponse } from '../types/api';
@@ -7,33 +6,32 @@ const api = ApiClient.getInstance();
 
 //upload url
 export const getPresignedUploadUrl = async (contentType: string): Promise<UploadResponse|null> => {
-    const res = await api.request<ApiResponse<UploadResponse>>(
-        `users/profile-image?contentType=${encodeURIComponent(contentType)}`,
-        {
-          method: 'GET',
-        });
-  
-    if (res.error) {
-      console.error("❌ presigned upload url 발급 실패:", res.error);
-      return null;
-    }
-
-    return res.data?.result || null;
-  };
+    try {
+        const res = await api.request<ApiResponse<UploadResponse>>(
+          `users/profile-image?contentType=${encodeURIComponent(contentType)}`,
+          { method: 'GET' }
+        );
+    
+        return res.data?.result ?? null;
+      } catch (error: any) {
+        console.error('❌ Presigned URL 발급 실패:', error.message || error);
+        return null;
+      }
+    };
 
 //다운로드 url
 export const getProfileImageUrl = async (): Promise<string | null> => {
-    const res = await api.request<ApiResponse<string>>("users/profile-image-url", {
-      method: "GET",
-    });
-  
-    if (res.error) {
-      console.error("❌ 프로필 이미지 조회 실패:", res.error);
-      return null;
-    }
-  
-    return res.data?.result|| null;
-  };
+    try {
+        const res = await api.request<ApiResponse<string>>('users/profile-image-url', {
+          method: 'GET',
+        });
+    
+        return res.data?.result ?? null;
+      } catch (error: any) {
+        console.error('❌ 프로필 이미지 조회 실패:', error.message || error);
+        return null;
+      }
+    };
 
   //s3 에 업로드
   export const uploadFileToS3 = async (uploadUrl: string, file: File): Promise<void> => {
