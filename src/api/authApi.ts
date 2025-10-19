b9220c32442898e2e67f824db8db6e3b696b98bd
dkfla <arim329@naver.com>
2025-10-19 17:22:25 +0900

    feat: 기본 선물 추천 연결

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -11,7 +11,7 @@ export class AuthApi {
 
   // 일반 로그인
   async normalLogin(credentials: LoginRequest) {
-    return await this.apiClient.request<LoginResponse>('api/auth/login', {
+    return await this.apiClient.request<LoginResponse>('auth/login', {
       method: 'POST',
       body: JSON.stringify(credentials),
     });
@@ -19,7 +19,7 @@ export class AuthApi {
 
   // 카카오 로그인
   async kakaoLogin(kakaoData: KakaoLoginRequest) {
-    return await this.apiClient.request<LoginResponse>(`api/auth/kakao?code=${kakaoData.code}`, {
+    return await this.apiClient.request<LoginResponse>(`auth/kakao?code=${kakaoData.code}`, {
       method: 'GET',
     });
   }
