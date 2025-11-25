cc0cf45beed1358b870b85441d91b0115c006d57
ddhi7 <tjsanf211323@gmail.com>
2025-11-26 02:19:22 +0900

    fix : 로고 변경

--- src/App.tsx
+++ src/App.tsx
@@ -27,29 +27,29 @@ function App() {
     <BrowserRouter>
       <Navbar />
       <Routes>
-      <Route path="/" element={<CalendarPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/api/auth/kakao/callback" element={<OAuthKakaoCallback />} />
-      <Route path="/calendar" element={<CalendarPage />} />
-      <Route path="/recommend" element={<RecommendPage/>} />
-      
-      <Route element={<ProtectedLayer />}>
-      <Route path="/recommend/result/:category" element={<RecommandResultTemplate />} />
-      <Route path="/friends" element={<FriendsPage />} />
-      <Route path="/friends/requests" element={<FriendRequestPage />} />
-      <Route path="/friend-profile/:friendId" element={<FriendProfilePage />} />
-      <Route path="/friend/:friendId" element={<FriendProfilePage />} />
-      <Route path="/friends/chat/*" element={<ChatPage />} />
-      <Route path="/friends/search" element={<FriendSearchPage />} />
-      <Route path="/mypage/*" element= {<MyPage/>}/>
-      </Route>  
+        <Route path="/" element={<CalendarPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/api/auth/kakao/callback" element={<OAuthKakaoCallback />} />
+        <Route path="/calendar" element={<CalendarPage />} />
+        <Route path="/recommend" element={<RecommendPage />} />
 
-      <Route path="/onboarding/profile" element={<OnboardingProfilePage />} />
-      <Route path="/onboarding/interests" element={<OnboardingInterestsPage />} />
-      <Route path="/onboarding" element={<Navigate to="/onboarding/profile" replace />} />
+        <Route element={<ProtectedLayer />}>
+          <Route path="/recommend/result/:category" element={<RecommandResultTemplate />} />
+          <Route path="/friends" element={<FriendsPage />} />
+          <Route path="/friends/requests" element={<FriendRequestPage />} />
+          <Route path="/friend-profile/:friendId" element={<FriendProfilePage />} />
+          <Route path="/friend/:friendId" element={<FriendProfilePage />} />
+          <Route path="/friends/chat/*" element={<ChatPage />} />
+          <Route path="/friends/search" element={<FriendSearchPage />} />
+          <Route path="/mypage/*" element={<MyPage />} />
+        </Route>
+
+        <Route path="/onboarding/profile" element={<OnboardingProfilePage />} />
+        <Route path="/onboarding/interests" element={<OnboardingInterestsPage />} />
+        <Route path="/onboarding" element={<Navigate to="/onboarding/profile" replace />} />
       </Routes>
-      <ToastContainer position="bottom-right" autoClose={1500} hideProgressBar newestOnTop closeOnClick pauseOnFocusLoss={false} draggable pauseOnHover theme="light"/>
+      <ToastContainer position="bottom-right" autoClose={1500} hideProgressBar newestOnTop closeOnClick pauseOnFocusLoss={false} draggable pauseOnHover theme="light" />
     </BrowserRouter>
   );
 }
